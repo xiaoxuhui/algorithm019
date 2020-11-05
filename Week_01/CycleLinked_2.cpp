cf1a10c3411a3e8166a59ebb4dda380f67dacda3
xiaoxuhui <xiaoxuhui@gridfocustech.com>
2020-11-06 00:13:17 +0800

    提交环形链表2的作业

---
+++ Week_01/CycleLinked_2.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *detectCycle(ListNode *head)
+    {
+        // if(head==NULL || head->next == NULL) {
+        //     return NULL;
+        // }
+        // ListNode *fastNode = head->next;
+        // ListNode *slowNode = head;
+        // while(fastNode != slowNode) {
+        //     if(fastNode == NULL || fastNode->next == NULL) {
+        //         return NULL;
+        //     }
+        //     fastNode = fastNode->next->next;
+        //     slowNode = slowNode->next;
+        // }
+        // ListNode *pNode = head;
+        // while(pNode != slowNode) {
+        //     pNode=pNode->next;
+        //     slowNode=slowNode->next;
+        // }
+        // return pNode;
+        ListNode *slow = head, *fast = head;
+        while (fast != nullptr)
+        {
+            slow = slow->next;
+            if (fast->next == nullptr)
+            {
+                return nullptr;
+            }
+            fast = fast->next->next;
+            if (fast == slow)
+            {
+                ListNode *ptr = head;
+                while (ptr != slow)
+                {
+                    ptr = ptr->next;
+                    slow = slow->next;
+                }
+                return ptr;
+            }
+        }
+        return nullptr;
+    }
+};
